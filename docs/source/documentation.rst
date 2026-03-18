6bddce4007638e96b823499664aeede66079275b
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2026-03-18 11:38:18 +0100

    Add irradiance colormap (#15)
    * Add irradiance colormap
    
    * Update function names + doc
    
    * Update function name
    
    * Update examples
    
    * Remove complete test requirement (temporarily)
    
    * Revert "Remove complete test requirement (temporarily)"
    
    This reverts commit 4f8e1cd3204d5d6828002cf80606dd5da3af1b14.
    
    * Fix doc examples
    
    * Downgrade sphinx
    
    * Silence plot output
    
    * Restore sphinx version

--- docs/source/documentation.rst
+++ docs/source/documentation.rst
@@ -8,5 +8,6 @@ Code documentation
    :toctree: generated/
 
    plotting.two_part_colormap
+   plotting.solar_colormap_and_norm
    plotting.plot_google_maps
    plotting.plot_intraday_heatmap
--- src/solarpy/plotting/__init__.py
+++ src/solarpy/plotting/__init__.py
@@ -1,3 +1,4 @@
-from solarpy.plotting.style import two_part_colormap  # noqa: F401
+from solarpy.plotting.colors import two_part_colormap  # noqa: F401
+from solarpy.plotting.colors import solar_colormap_and_norm  # noqa: F401
 from solarpy.plotting.map import plot_google_maps  # noqa: F401
 from solarpy.plotting.intraday_heatmap import plot_intraday_heatmap  # noqa: F401
